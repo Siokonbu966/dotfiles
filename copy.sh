d52685c18eb2b7a38dcc8a7bf20d1098ffed3247
Siokonbu966 <167207736+Siokonbu966@users.noreply.github.com>
2025-10-10 09:22:47 +0900

    remove oh-my-zsh dirctory

--- copy.sh
+++ copy.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+# vim
 cp ~/.vimrc ./
+
+# nvim
 cp ~/.config/nvim/init.lua ./nvim
 cp ~/.config/nvim/lazy-lock.json ./nvim
 cp -r ~/.config/nvim/lua ./nvim/
+
+# neofetch
 cp -r ~/.config/neofetch ./
+
+# oh-my-zsh
+cp ~/.p10k.zsh ./
+cp -r ~/.oh-my-zsh ./
