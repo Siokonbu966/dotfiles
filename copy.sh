ad2e455b6b3235a60acc62175e940afb73e957c7
Siokonbu966 <167207736+Siokonbu966@users.noreply.github.com>
2025-10-19 21:26:33 +0900

    remove vimrc & oh-my-zsh

--- copy.sh
+++ copy.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
-# vim
-cp ~/.vimrc ./
 
 # nvim
 cp ~/.config/nvim/init.lua ./nvim
 cp ~/.config/nvim/lazy-lock.json ./nvim
 cp -r ~/.config/nvim/lua ./nvim/
 
-# neofetch
-cp -r ~/.config/neofetch ./
-
-# oh-my-zsh
-cp ~/.p10k.zsh ./
-cp -r ~/.oh-my-zsh ./
