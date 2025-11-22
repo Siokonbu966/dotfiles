0875ff6f38b0880233588ab715827113e047a77e
Siokonbu966 <167207736+Siokonbu966@users.noreply.github.com>
2025-11-22 22:00:00 +0900

    copy.shをトラッキングから除外

--- copy.sh
+++ copy.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-
 # nvim
 cp -r ~/.config/nvim ./nvim/
 
+# neofetch
+cp -r ~/.config/neofetch ./
+
+# sketchybar
+cp -r ~/.config/sketchybar ./
+
+# karabiner
+cp -r ~/.config/karabiner ./
+
+# ghostty
+cp -r ~/.config/ghostty ./
