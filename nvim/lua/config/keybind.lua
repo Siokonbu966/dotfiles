8dfcb93f0d57510f3bb9235c09dc581f41f3ca91
Siokonbu966 <167207736+Siokonbu966@users.noreply.github.com>
2026-01-10 18:55:55 +0900

    nvim: change keymap

--- nvim/lua/config/keybind.lua
+++ nvim/lua/config/keybind.lua
@@ -1,5 +1,12 @@
+-- telescope
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+-- move
+vim.keymap.set('n', 'k', ':normal! 1h<CR>', { silent = true })
+vim.keymap.set('n', 't', ':normal! 1k<CR>', { silent = true })
+vim.keymap.set('n', 'n', ':normal! 1j<CR>', { silent = true })
+vim.keymap.set('n', 's', ':normal! 1l<CR>', { silent = true })
